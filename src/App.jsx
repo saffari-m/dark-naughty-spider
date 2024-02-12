b4560a18ca6ca01b77055e4385d791dfcb582188
Iman <aaarjmandnia@gmail.com>
2024-02-12 17:19:18 +0330

    fix: :zap: Fixed some issues of not loading the project in old browsers

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Router, Route } from "@solidjs/router";
 
 import Layout from "./components/Layout";
 import Home from "./routes";
-import About from "./routes/about";
+// import About from "./routes/about";
 
 import "./index.css";
 
@@ -11,7 +11,8 @@ export default function App() {
   return (
     <Router root={Layout}>
       <Route path="/" component={Home} />
-      <Route path="/about" component={About} />
+      {/* <Route path="/about" component={About} /> */}
     </Router>
   );
 }
+import "./proxy-pollyfills";
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import { babel } from '@rollup/plugin-babel';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import { babel } from "@rollup/plugin-babel";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
   plugins: [
     solidPlugin(),
+    legacy({
+      targets: ">0.1%, defaults, supports es5",
+      renderModernChunks: false,
+      additionalLegacyPolyfills: false,
+      polyfills: false,
+      modernPolyfills: false,
+      renderLegacyChunks: true,
+    }),
   ],
   server: {
     port: 3000,
@@ -13,30 +22,35 @@ export default defineConfig({
     minify: false, // "terser",
     rollupOptions: {
       output: {
-        format: 'cjs',
-        generatedCode: 'es5',
+        format: "iife",
+        generatedCode: "es5",
       },
       plugins: [
         babel({
           // exclude: 'node_modules/**', // specify exaclty wich one do we need like solid js
-          babelHelpers: 'bundled',
+          babelHelpers: "bundled",
           presets: [
-            ['@babel/preset-env',  {
-              modules: false,
-              useBuiltIns: 'entry',
-              targets: {
-                  chrome: '32',
-                  firefox: '40',
-                  edge: '16'
-              }
-          }]
+            [
+              "@babel/preset-env",
+              {
+                modules: false,
+                useBuiltIns: "entry",
+                // targets: {
+                //   chrome: "32",
+                //   firefox: "40",
+                //   edge: "16",
+                // },
+                targets: "> 0.25%, not dead",
+                corejs: "3",
+              },
+            ],
           ],
-          plugins: [ ], //"@babel/plugin-transform-runtime" // ["@babel/plugin-transform-arrow-functions"]
+          plugins: [], //"@babel/plugin-transform-runtime" // ["@babel/plugin-transform-arrow-functions"]
         }),
       ],
     },
     terserOptions: {
-      ecma: '5',
+      ecma: "5",
       compress: true,
       mangle: false,
     },
